5adc949e92161ba4aa4dc3e5a1880f7f35be7278
oowaki shouko <202412ap01_oowaki@x-ide.sakura.ne.jp>
2025-02-17 14:46:30 +0900

    #35765　机上金額のエラーメッセージが表示されない事象を修正しました。

--- SRC/js/stock.js
+++ SRC/js/stock.js
@@ -63,7 +63,7 @@ function fnStockEditCheck() {
 	if (!fnYMDCheck("正しい内見日付", form.visitDT)) { return; }
 
 	tmp = form.deskPrice.value;
-	if (tmp.length > 5 || tmp.match(/[^0-9]+/)) {
+	if (tmp.length > 5 || tmp.match(/[^0-9]/)) {
 		alert('机上金額は5桁以内の半角数字で入力してください');
 		return;
 	}
